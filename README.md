d9c03141a4e32f29d12f175727af08d52dd432a0
Conner McCabe <conner.p.mccabe@gmail.com>
2020-07-17 15:14:29 -0500

    added is palindrome

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-New Readme for algorithm practice folder
-
+An Algorithm a day keeps the Corona at bay
 ### D1- PigLatin
-    an function that takes a string
+    a function that takes a string, and returns a string that is split in half, reversed in order, and with '-ay' at the end
+    ex; pigLt('bolster') => 'olsterbay'
+### D2- Palindromes
+    a function that takes a string, to sees if the letters are the same order if reversed, returns a boolean
+    ex; isPalindrome('taco cat') => true
--- js-intro.js
+++ js-intro.js
@@ -4,5 +4,25 @@
         one problem, do it over and over again on a smaller or changing piece until you hit an end point, the base case
     2 components that make up a recursive function
     How to visualize the call stack to better understand and debug recursive functions
-
 */
+//PigLatin
+function pigLt(str) {
+    let normalArr = str.split(' ');
+    let pigArr = [];
+    newWord = '';
+    for(let word in normalArr) {
+        let newWord = normalArr[word].substring(1) + normalArr[word].substring(0, 1) + 'ay';
+        pigArr.push(newWord);
+    }
+    return pigArr.join(' ');
+}
+console.log(pigLt('bolster'));
+console.log(pigLt('cannon'));
+
+//is palindrome
+function isPalindrome(str) {
+    str = str.toLowerCase();
+    return (str == str.split('').reverse().join(''));
+}
+console.log(isPalindrome('level'));
+console.log(isPalindrome('A car, a man, a maraca'));
