4ff7c1a159948e2326987f4e2ba013ffd59f2ae6
Conner McCabe <conner.p.mccabe@gmail.com>
2020-09-30 16:25:40 -0500

    adding romanize function for D10

--- js-intro.js
+++ js-intro.js
@@ -190,3 +190,16 @@ let a = ['goose', 'geese', 'meece']
 
 console.log(toWeirdCase( "String" ));//=> returns "StRiNg"
 console.log(toWeirdCase( "Weird string case" ));//=> returns "WeIrD StRiNg cAsE"
+
+function romanize(num) {
+    let lookup = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1},
+    roman = '',
+    i;
+    for (i in lookup) {
+        while (num >= lookup[i]) {
+            roman += 1;
+            num -= lookup[i];
+        }
+    }
+    return roman;
+}
