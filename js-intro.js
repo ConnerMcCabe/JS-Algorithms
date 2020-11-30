e4e57bb2df8e5a6a989ad8fd9ddadd4f55e26df9
Conner McCabe <conner.p.mccabe@gmail.com>
2020-11-30 13:08:07 -0600

    adding alternate solution to first Recurring Character

--- js-intro.js
+++ js-intro.js
@@ -205,7 +205,7 @@ function romanize(num) {
 }
 
 function firstRecurringCharacter(input) {
-    for (let i =0; i < input.length; i++) {
+    for (let i = 0; i < input.length; i++) {
         for (let j = i + 1; j < input.length; j++) {
             if (input[i] === input[j]) {
                 return input[i]
@@ -215,3 +215,17 @@ function firstRecurringCharacter(input) {
 }
 
 firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
+
+function fRC2(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        if (map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i
+        }
+        console.log(map)
+    }
+    return undefined;
+}
+fRC2([2,5,1,2,3,5,1,2,4]);
